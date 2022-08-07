60ba7c7365b14c7a11fe31e6d176063aa1b45456
Manjur Morshed <40655280+theMorshed@users.noreply.github.com>
2022-08-07 14:08:05 +0600

    solved join string problem

---
+++ joinString.js
@@ -0,0 +1,26 @@
+/*
+John’s teacher gave him two variables. Each variable contains a string. John’s teacher asked him to
+combine these two strings and print them in one line. Help John write the program.
+Input:
+The first and the second lines of the input contain the strings.
+Output:
+Print the result in one line.
+
+Practice Problem 3
+
+Sample Input:
+I am going to be
+an awesome web developer
+Output:
+I am going to be an awesome web developer
+*/
+
+// declare variable and initialize
+var stringOne = 'I am going to be';
+var stringTwo = ' an awesome web developer';
+
+// join both of string
+var completeString = stringOne + stringTwo;
+
+// print result
+console.log(completeString);
